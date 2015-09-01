e4e3523087edde7ad102c5df464d7760c0021b6b
Idris Y <sld7700@gmail.com>
2015-09-01 17:33:10 +0300

    Added apps installation by using brew.

--- install.sh
+++ install.sh
@@ -1,15 +1,37 @@
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-# Git
-echo 'Symlinking git dotfiles'
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+function symlink_dotfiles() {
+  # Git
+  echo 'Symlinking git dotfiles'
+  ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+  ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
 
-# Zsh
-echo 'Symlinking zsh dotfiles'
-ln -sfv "$DOTFILES_DIR/zsh/.zshrc" ~
+  # Zsh
+  echo 'Symlinking zsh dotfiles'
+  ln -sfv "$DOTFILES_DIR/zsh/.zshrc" ~
+}
+
+function install_brew() {
+  which -s brew
+  if [[ $? != 0 ]]; then
+    echo 'Installing Homebrew...'
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew tap caskroom/cask
+    brew install brew-cask
+    brew tap caskroom/versions
+    brew update
+    brew doctor
+  fi
+}
+
+function install_rvm() {
+  curl -L https://get.rvm.io | bash -s stable --auto-dotfiles --autolibs=enable --rails
+}
+
+
+symlink_dotfiles()
 
 
 # /etc/hosts
@@ -17,20 +39,32 @@ echo 'Copying /etc/hosts'
 sudo cp etc/hosts /etc/hosts
 
 
-# ssh settings
-# make it by hand
-
-
 # If we on OS X, install homebrew and tweak system a bit.
 if [[ `uname` == 'Darwin' ]]; then
-  which -s brew
-  if [[ $? != 0 ]]; then
-    echo 'Installing Homebrew...'
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew update
-    brew install htop mysql nginx node ruby
-  fi
+  xcode-select --install
+
+  install_brew()
+
+  brew install htop nano wget
+  brew install git
+  brew install nginx node
+  brew install pyenv
+
+  install_rvm()
+  rvm install ruby-2.1.1p76
+
+  brew cask install dropbox
+  brew cask install firefox
+  brew cask install sublime-text3
+
+  brew cask install dash divvy
+
+  brew cask install libreoffice
+  brew cask install toggl slack
+  brew cask install skype
+  brew cask install iterm2
 fi
+
 
 
 echo 'Applying sublime config...'
